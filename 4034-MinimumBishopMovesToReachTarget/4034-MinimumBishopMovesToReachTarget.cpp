b3ad565362cf0001a24b7b6f0d1ff153e0e760bd
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-09-04 22:00:25 +0530

    Create 4034-MinimumBishopMovesToReachTarget

---
+++ 4034-MinimumBishopMovesToReachTarget/4034-MinimumBishopMovesToReachTarget.cpp
@@ -0,0 +1,10 @@
+// Last updated: 9/4/2026, 10:00:24 PM
+class Solution {
+public:
+    int minBishopMoves(vector<int>& s, vector<int>& t) {
+        if (s[0] == t[0] && s[1] == t[1]) return 0;
+        if((s[0]+s[1])%2 != (t[0]+t[1])%2) return -1;
+        if(abs(s[0] - t[0]) == abs(s[1] - t[1])) return 1;
+        return 2;
+    }
+};
